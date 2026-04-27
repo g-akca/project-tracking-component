e577388fe1d9eeb4cbce6f8c08b8acc7bed5ddd6
g-akca <gok.akca66@gmail.com>
2026-04-27 22:40:12 +0300

    Create MainSection structure

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,7 +2,7 @@ import illustration from "/images/illustration-devices.svg";
 
 function HeroImage() {
   return (
-    <img src={illustration} alt="Devices illustration" className="max-w-none w-[132vw] -translate-x-4 mt-16 mb-21.75" />
+    <img src={illustration} alt="Devices illustration" className="max-w-none w-[132vw] -translate-x-5.5 mt-16 mb-21.75" />
   )
 }
 
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,23 @@
 function MainSection() {
   return (
-    <main>
-      
+    <main className="flex flex-col gap-8">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4">
+            <span>New</span>
+            <p>Monograph Dashboard</p>
+          </div>
+
+          <h1>Powerful Insights Into Your Team</h1>
+        </div>
+
+        <p>Project planning and time tracking for agile teams</p>
+      </div>
+
+      <div className="flex gap-4">
+        <button type="button">Schedule a Demo</button>
+        <p>To see a preview</p>
+      </div>
     </main>
   )
 }
