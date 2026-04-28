fcff1dc0d91bf675438a612397a96eaf75e5db01
g-akca <gok.akca66@gmail.com>
2026-04-28 15:35:15 +0300

    Show close or open button based on menu state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import logo from "/images/logo.svg";
 import hamburgerIcon from "/images/icon-hamburger.svg";
+import closeIcon from "/images/icon-close.svg";
 import Menu from "./Menu";
 
 import { useState } from "react";
@@ -10,7 +11,15 @@ function Header() {
   return (
     <header className="flex justify-between items-center">
       <img src={logo} alt="Logo image" className="h-6" />
-      <img src={hamburgerIcon} alt="Hamburger icon" />
+      {!isMenuOpen ? (
+        <button type="button" onClick={() => setIsMenuOpen(true)}>
+          <img src={hamburgerIcon} alt="Hamburger icon" />
+        </button>
+      ) : (
+        <button type="button" onClick={() => setIsMenuOpen(false)}>
+          <img src={closeIcon} alt="Close icon" />
+        </button>
+      )}
 
       {isMenuOpen && 
         <Menu />
