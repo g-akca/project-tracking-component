69e73e8fccf3a634bcdb8a481f649b7b24193fee
g-akca <gok.akca66@gmail.com>
2026-04-28 20:57:43 +0300

    Add more top padding

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div className="relative text-base leading-base text-blue-950 pt-9.5 pb-16 px-8 tablet:px-20 overflow-hidden">
+    <div className="relative text-base leading-base text-blue-950 pt-9.5 pb-16 desktop:py-16 px-8 tablet:px-20 overflow-hidden">
       <BackgroundMask />
 
       <div className="flex flex-col gap-16 tablet:gap-15.75 desktop:gap-31 max-w-277.5 mx-auto">
