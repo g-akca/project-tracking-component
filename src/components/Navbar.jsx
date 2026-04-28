c3b9b334429d12996595710f21f8af306247bbec
g-akca <gok.akca66@gmail.com>
2026-04-28 21:26:20 +0300

    Add Navbar structure and styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 function Navbar() {
   return (
-    <nav className="hidden desktop:flex gap-10">
-
+    <nav className="hidden desktop:block">
+      <ul className="flex gap-10 items-center font-bold uppercase tracking-[1px]">
+        <li><a href="#">Product</a></li>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Pricing</a></li>
+        <div className="w-1.5 aspect-square rounded-full bg-blue-950 opacity-15" />
+        <li className="opacity-50"><a href="#">Login</a></li>
+      </ul>
     </nav>
   )
 }
