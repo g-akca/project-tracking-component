39404d865fb824654094493aa5fdda89d80efd0e
g-akca <gok.akca66@gmail.com>
2026-04-28 20:36:18 +0300

    Add tablet and desktop responsive Menu styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,6 @@
 function Menu() {
   return (
-    <nav className="absolute z-50 -bottom-70 w-full p-6 bg-white rounded-[3px] shadow-[0_5px_10px_rgba(0,0,0,0.15)]">
+    <nav className="desktop:hidden absolute z-50 -bottom-70 w-full p-6 tablet:px-11 bg-white rounded-[3px] shadow-[0_5px_10px_rgba(0,0,0,0.15)]">
       <ul className="flex flex-col gap-6 items-center font-bold text-[18px] leading-[125%] uppercase tracking-[1.2px]">
         <li><a href="#">Product</a></li>
         <li><a href="#">Features</a></li>
