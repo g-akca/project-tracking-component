83bc9fcbda9f1f64b4c608f8a56d502ae9511991
g-akca <gok.akca66@gmail.com>
2026-04-28 20:57:09 +0300

    Add desktop responsive App styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
     <div className="relative text-base leading-base text-blue-950 pt-9.5 pb-16 px-8 tablet:px-20 overflow-hidden">
       <BackgroundMask />
 
-      <div className="flex flex-col gap-16 tablet:gap-15.75 max-w-277.5 mx-auto">
+      <div className="flex flex-col gap-16 tablet:gap-15.75 desktop:gap-31 max-w-277.5 mx-auto">
         <Header />
 
-        <div className="flex flex-col gap-21.75 tablet:gap-16">
+        <div className="flex flex-col desktop:flex-row-reverse gap-21.75 tablet:gap-16">
           <HeroImage />
           <MainSection />
         </div>
